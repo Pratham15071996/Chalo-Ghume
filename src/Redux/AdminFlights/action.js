44dceb1af70852a47b1f941a11ad3f81dbb5debb
Pratham15071996 <prathamsinha1000@gmail.com>
2026-05-14 09:24:55 +0530

    first commit

---
+++ src/Redux/AdminFlights/action.js
@@ -0,0 +1,76 @@
+import axios from "axios";
+import {
+  DELETE_FLIGHTS,
+  FETCH_FLIGHTS,
+  FLIGHT_FAILURE,
+  FLIGHT_REQUEST,
+  GET_FLIGHT_SUCCESS,
+  POST_FLIGHT_SUCCESS,
+} from "./actionType";
+
+export const getFlightSuccess = (payload) => {
+  return { type: GET_FLIGHT_SUCCESS, payload };
+};
+
+export const postFlightSuccess = (payload) => {
+  return { type: POST_FLIGHT_SUCCESS };
+};
+
+export const flightRequest = () => {
+  return { type: FLIGHT_REQUEST };
+};
+
+export const flightFailure = () => {
+  return { type: FLIGHT_FAILURE };
+};
+
+//
+export const fetch_flights_product = (payload) => {
+  return { type: FETCH_FLIGHTS, payload };
+};
+//
+export const handleDeleteProduct = (payload) => {
+  return { type: DELETE_FLIGHTS, payload };
+};
+
+export const addFlight = (payload) => (dispatch) => {
+  dispatch(flightRequest());
+
+  axios
+    .post("https://happy-sunglasses-eel.cyclic.app/flight", payload) // https://makemytrip-api-data.onrender.com/flight
+    .then(() => {
+      dispatch(postFlightSuccess());
+    })
+    .catch((err) => {
+    });
+};
+
+//
+export const fetchFlightProducts = (limit) => (dispatch) => {
+  dispatch(flightRequest());
+  axios
+    .get(`https://happy-sunglasses-eel.cyclic.app/flight?_limit=${limit}`)   //https://makemytrip-api-data.onrender.com/flight?_limit=${limit}
+    .then((res) => {
+      dispatch(fetch_flights_product(res.data));
+    })
+    .catch((err) => {
+    });
+};
+
+export const DeleteFlightProducts = (deleteId) => async (dispatch) => {
+  try {
+    const res = await axios(
+      `https://happy-sunglasses-eel.cyclic.app/flight?${deleteId}`, //https://makemytrip-api-data.onrender.com/flight/${deleteId}
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    let data = await res.json();
+
+    dispatch(handleDeleteProduct(deleteId));
+  } catch (e) {
+  }
+};
